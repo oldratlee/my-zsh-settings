a2c1e7acc97234bab89d15ac2be6730847837d5a
Jerry Lee <oldratlee@gmail.com>
2015-12-18 02:06:21 +0800

    group settings; fix grw

--- zsh-settings-for-oldratlee.plugin.zsh
+++ zsh-settings-for-oldratlee.plugin.zsh
@@ -1,9 +1,6 @@
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=128m"
 #export GRADLE_OPTS="-Xmx1024m -Xms256m -XX:MaxPermSize=512m"
 export SVN_EDITOR=vim
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export ANDROID_SDK_HOME=$ANDROID_HOME
-export ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle
 
 CLISP_DOC=/usr/local/Cellar/clisp/2.49/share/doc/clisp/doc
 
@@ -18,28 +15,57 @@ fi
 
 export LESS="${LESS}i"
 
+###############################################################################
+# Shell misc
+###############################################################################
+alias d="dirs -v | head | tr '\t' ' ' | colines"
+
 alias wa='which -a'
 alias ta='type -a'
+alias o=open
 alias du='du -h'
 alias df='df -h'
 alias ll='ls -lh'
 alias tailf='tail -f'
-alias grep='grep --color=auto --exclude-dir=.cvs --exclude-dir=.git --exclude-dir=.hg --exclude-dir=.svn --exclude-dir=target --exclude-dir=.idea'
-#alias grep='grep --color=auto --exclude-dir=.cvs --exclude-dir=.git --exclude-dir=.hg --exclude-dir=.svn --exclude-dir=target --exclude-dir=.idea --exclude=\*.jar --exclude=\*.ipr --exclude=\*.iml --exclude=\*.iws'
+# alias grep='grep --color=auto --exclude-dir=.cvs --exclude-dir=.git --exclude-dir=.hg --exclude-dir=.svn --exclude-dir=target --exclude-dir=.idea'
+alias grep='grep --color=auto --exclude-dir=.cvs --exclude-dir=.git --exclude-dir=.hg --exclude-dir=.svn --exclude-dir=target --exclude-dir=build --exclude-dir=.idea --exclude=\*.ipr --exclude=\*.iml --exclude=\*.iws --exclude=\*.jar'
 alias diff=colordiff
 
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
+alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
+
+alias cap='c ap'
+alias v=vim
+alias vi=vim
+alias gv=gvim
+alias sl='open -a /Applications/Sublime\ Text\ 2.app'
+
 alias a='atom'
+
+###############################################################################
+# Python
+###############################################################################
 alias py='python'
 alias py3='python3'
 alias ipy='ipython'
 alias ipy3='ipython3'
 
+###############################################################################
+# Prolog
+###############################################################################
 alias gpl='gprolog'
 alias sp='/Applications/SWI-Prolog.app/Contents/MacOS/swipl'
 alias bp='/Users/jerry/ProgFiles/BProlog/bp'
+
+###############################################################################
+# Lisp
+###############################################################################
 alias schm='rlwrap -p 1\;32 -r -c -f $HOME/.scheme_completion.rlwrap scheme'
 alias srepl='scala -nc -Dscala.color'
 
+###############################################################################
+# Gradle
+###############################################################################
 # --daemon
 alias grd='gradle'
 function grw() {
@@ -50,9 +76,9 @@ function grw() {
             return 1
         }
         [ -f "$d/gradlew" ] && {
-            d=`dirname "$d"`
             break
         }
+        d=`dirname "$d"`
     done
 
     [ $d != $PWD ] && echo "use gradle wrapper: $(realpath "$d" --relative-to="$PWD")/gradlew"
@@ -75,6 +101,9 @@ alias grwdtc='grw -q dependencies --configuration testCompile'
 alias kgrdm="jps -l | awk '\$2==\"org.gradle.launcher.daemon.bootstrap.GradleDaemon\"{print \$1}' | xargs -r kill -9"
 alias sgrdm="jps -l | awk '\$2==\"org.gradle.launcher.daemon.bootstrap.GradleDaemon\"{print \$0}'"
 
+###############################################################################
+# Git
+###############################################################################
 alias ga.='git add .'
 alias ga..='git add ..'
 alias gdc='git diff --cached'
@@ -92,16 +121,9 @@ alias gamno='git commit --amend --no-edit'
 alias gpf='git push -f'
 alias gampf='git commit --amend --no-edit && git push -f'
 
-alias d="dirs -v | head | tr '\t' ' ' | colines"
-
-alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
-alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
-
-alias o=open
-alias cap='c ap'
-alias vi=vim
-alias sl='open -a /Applications/Sublime\ Text\ 2.app'
-
+###############################################################################
+# JetBrains
+###############################################################################
 alias idea='open -a /Applications/IntelliJ\ IDEA\ 15.app'
 alias ads='open -a /Applications/Android\ Studio.app'
 alias adsp='open -a /Applications/AndroidStudioPreview.app'
