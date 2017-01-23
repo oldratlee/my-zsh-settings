1ddf1d1f6c8715b35280693022e038bf9a4bb494
Jerry Lee <oldratlee@gmail.com>
2017-01-23 22:07:51 +0800

    reorder sections

--- my-zsh-settings.plugin.zsh
+++ my-zsh-settings.plugin.zsh
@@ -2,7 +2,6 @@
 # Env settings
 ###############################################################################
 
-export SVN_EDITOR=vim
 export EDITOR=vim
 #export SHELL=/bin/bash
 export LANG=en_US.UTF-8
@@ -124,13 +123,189 @@ toc() {
     command doctoc --notitle "$@" && sed '/<!-- START doctoc generated TOC/,/<!-- END doctoc generated TOC/s/^( +)/\1\1/' -ri "$@"
 }
 
+
 ###############################################################################
-# Java
+# Git
 ###############################################################################
 
-export MAVEN_OPTS="-Xmx512m"
+compdef g=git
+
+# git diff
+
+alias gdc='git diff --cached'
+function gds() {
+    if [ $# -eq 0 ]; then
+        from=@^
+        to=@
+    elif [ $# -eq 1 ]; then
+        from="$1^"
+        to="$1"
+    else
+        from="$1"
+        to="$2"
+    fi
+    git diff "$from" "$to"
+}
+alias gdh='git diff HEAD'
+alias gdorigin='git diff origin/$(git_current_branch)'
+
+# git status
+
+alias gssi='git status -s --ignored'
+alias gsti='git status --ignored'
+
+# git log
+
+alias gg='glog -15'
+
+## git branch
+
+gbb() {
+    git branch -a "$@" | sed "/->/b; s#remotes/origin/#remotes/origin => #"
+}
+
+gbT() {
+    # http://stackoverflow.com/questions/5188320/how-can-i-get-a-list-of-git-branches-ordered-by-most-recent-commit
+    # --sort=-committerdate : sort branch by commit date in descending order
+    # --sort=committerdate : sort branch by commit date in ascending order
+    git branch -a --sort=committerdate "$@" | sed -r "/->/b; /\/tags\//d; /\/releases\//d; /\/backups?\//d; s#remotes/origin/#remotes/origin => #"
+}
+
+# http://stackoverflow.com/questions/1419623/how-to-list-branches-that-contain-a-given-commit
+alias gbc='git branch --contains'
+alias gbrc='git branch -r --contains'
+
+alias gbd='git branch -d'
+alias gbD='git branch -D'
+
+# git add
+
+alias ga.='git add .'
+alias ga..='git add ..'
+
+# git checkout
+
+alias gcoh='git checkout HEAD'
+
+# git reset/rebase
+
+alias grb='git rebase'
+alias grs='git reset'
+alias grshd='git reset --hard'
+alias grsorigin='git reset --hard origin/$(git_current_branch)'
+
+# git clone
+
+alias gcn='git clone'
+alias gcnr='git clone --recurse-submodules'
+
+# git commit
+
+alias gam='git commit --amend -v'
+alias gamno='git commit --amend --no-edit'
+
+# git push
+
+alias gpf='git push -f'
+
+# compound git command
+
+alias ga.c='git add . && git commit -v'
+alias ga.m='git add . && git commit --amend -v'
+alias ga.mno='git add . && git commit --amend --no-edit'
+
+alias ga.cp='git add . && git commit -v && git push'
+
+alias gampf='git commit --amend --no-edit && git push -f'
+alias ga.mpf='git add . && git commit --amend --no-edit && git push -f'
+
+# misc
+alias gbw='git browse'
+alias sg='open -a /Applications/SmartGit.app'
+
+
+## URL shower/switcher
+
+# show swithed git repo addr(git <=> http)
+shg() {
+    local url="${1:-$(git remote get-url origin)}"
+    if [ -z "$url" ]; then
+        echo "No arguement and Not a git repository!"
+        return 1
+    fi
+
+    if [[ "$url" =~ '^http' ]]; then
+        echo "$url" | sed 's#^https?://#git@#; s#(\.com|\.org)/#\1:#; s#(\.git)?$#\.git#' -r | c
+    else
+        echo "$url" | sed 's#^git@#http://#; s#http://github.com#https://github.com#; s#(\.com|\.org):#\1/#; s#\.git$##' -r | c
+    fi
+}
+# show git repo addr http addr recursively
+shgr() {
+    local d
+    for d in `find -iname .git -type d`; do
+        (
+            cd $d/..
+            echo "$PWD : $(git remote get-url origin)"
+        )
+    done
+}
+# change git repo addr http addr recursively
+chgr() {
+    local d
+    for d in `find -iname .git -type d`; do
+        (
+            cd $d/..
+            local url=$(git remote get-url origin)
+            [[ "$url" =~ '^http'  ]] && {
+                local gitUrl=$(sgh)
+                echo "CHANGE $PWD : $url to $gitUrl"
+                git remote set-url origin $gitUrl
+            } || {
+                echo "Ignore $PWD : $url"
+            }
+        )
+    done
+}
+
+
+# git up
+alias gu='git up'
+# git up recursively
+gur() {
+    local d
+    local -a failedDirs=()
+    for d in `find -iname .git -type d`; do
+        d="$(readlink -f "$d/..")"
+        (
+            cd $d && {
+                echo
+                echo "================================================================================"
+                echo "$PWD"
+                echo "================================================================================"
+                git up
+            }
+        ) || failedDirs=( "$failedDirs[@]" "$d")
+    done
+    if [ "$#failedDirs[@]" -gt 0 ]; then
+        echo
+        echo
+        echo "================================================================================"
+        echo "Failed dirs:"
+        for d in "$failedDirs[@]"; do
+            echo "    $d"
+        done
+    fi
+}
+
+
+###############################################################################
+# Java/JVM Languages
+###############################################################################
 
 swJavaNetProxy() {
+    # How to check if a variable is set in Bash?
+    # http://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
     [ -z "${JAVA_OPTS_BEFORE_NET_PROXY+if_undefined_will_output}" ] && {
         export JAVA_OPTS_BEFORE_NET_PROXY="$JAVA_OPTS"
         export JAVA_OPTS="$JAVA_OPTS -DproxySet=true -DsocksProxyHost=127.0.0.1 -DsocksProxyPort=7070"
@@ -142,11 +317,27 @@ swJavaNetProxy() {
     }
 }
 
+#export JAVA_HOME=$(/usr/libexec/java_home -v 1.6)
+export JAVA6_HOME='/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home'
+export JAVA7_HOME=$(echo /Library/Java/JavaVirtualMachines/jdk1.7.0_*.jdk/Contents/Home)
+export JAVA8_HOME=$(echo /Library/Java/JavaVirtualMachines/jdk1.8.0_*.jdk/Contents/Home)
+export JAVA9_HOME='/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home'
+
+export JAVA_HOME="$JAVA7_HOME"
+
+# JAVA_HOME switcher
 alias j6='export JAVA_HOME=$JAVA6_HOME'
 alias j7='export JAVA_HOME=$JAVA7_HOME'
 alias j8='export JAVA_HOME=$JAVA8_HOME'
+alias j9='export JAVA_HOME=$JAVA9_HOME'
 
-## Maven
+alias scl='scala -Dscala.color -feature'
+
+###############################################################################
+# Maven
+###############################################################################
+
+export MAVEN_OPTS="-Xmx512m"
 
 alias mc='mvn clean'
 alias mi='mvn install -Dmaven.test.skip -Dautoconf.skip -Dautoconfig.skip -Denv=release -Dscm.app.name=faked'
@@ -166,36 +357,66 @@ muv() {
 
 
 ###############################################################################
-# Erlang
+# Gradle
 ###############################################################################
 
-alias r2=rebar
-compdef r2=rebar
-alias r3=rebar3
-# compdef r3=rebar3
+#export GRADLE_OPTS="-Xmx1024m -Xms256m -XX:MaxPermSize=512m"
+# --daemon
+alias grd='gradle'
 
-# Run erlang MFA(Module-Function-Args) conveniently
-erun() {
-    if [ $# -lt 2 ]; then
-        echo "Error: at least 2 args!"
-        return 1
-    fi
-    erl -s "$@" -s init stop -noshell
+# shortcut for executing gradlew:
+# find gradlew automatically and execute.
+function grw() {
+    local d="$PWD"
+    while true; do
+        [ "/" = "$d" ] && {
+            echo "fail to find gradlew" 2>&1
+            return 1
+        }
+        [ -f "$d/gradlew" ] && {
+            break
+        }
+        d=`dirname "$d"`
+    done
+
+    [ $d != $PWD ] && echo "use gradle wrapper: $(realpath "$d" --relative-to="$PWD")/gradlew"
+    "$d/gradlew" "$@"
 }
 
-# Run erlang one-line script conveniently
-erline() {
-    if [ $# -ne 1 ]; then
-        echo "Error: Only need 1 arg!"
-        return 1
-    fi
-    erl -eval "$1" -s init stop -noshell
-}
+alias grwb='grw build'
+alias grwc='grw clean'
+alias grwcb='grw clean build'
+alias grwt='grw test'
+
+alias grwf='grw --refresh-dependencies'
+alias grwfb='grw --refresh-dependencies build'
+alias grwfc='grw --refresh-dependencies clean'
+alias grwfcb='grw --refresh-dependencies clean build'
+
+alias grwd='grw -q dependencies'
+alias grwdc='grw -q dependencies --configuration compile'
+alias grwdr='grw -q dependencies --configuration runtime'
+alias grwdtc='grw -q dependencies --configuration testCompile'
+
+# kill all gradle deamon processes on mac
+alias kgrdm="jps -l | awk '\$2==\"org.gradle.launcher.daemon.bootstrap.GradleDaemon\"{print \$1}' | xargs -r kill -9"
+# show all gradle deamon processes on mac
+alias sgrdm="jps -l | awk '\$2==\"org.gradle.launcher.daemon.bootstrap.GradleDaemon\"{print \$0}'"
+
+
+###############################################################################
+# Javascript
+###############################################################################
+
+# NVM: https://github.com/creationix/nvm
+export NVM_DIR="$HOME/.nvm"
+. "/usr/local/opt/nvm/nvm.sh"
 
 ###############################################################################
 # Python
 ###############################################################################
 
+# default python 2
 alias py='python'
 alias py2='python2'
 alias py3='python3'
@@ -211,6 +432,7 @@ alias pip3='pip3 --trusted-host pypi.douban.com'
 compdef pip=pip
 compdef pip2=pip
 compdef pip3=pip
+
 pipup() {
     pip list --outdated | awk 'NR>2{print $1}' | xargs pip install --upgrade
 }
@@ -248,12 +470,34 @@ relink_virtualenv() {
     )
 }
 
+
 ###############################################################################
-# Prolog
+# Erlang
 ###############################################################################
-alias sp='/Applications/SWI-Prolog.app/Contents/MacOS/swipl'
-alias gpl='gprolog'
-alias bp='$HOME/ProgFiles/BProlog/bp'
+
+alias r2=rebar
+compdef r2=rebar
+alias r3=rebar3
+# compdef r3=rebar3
+
+# Run erlang MFA(Module-Function-Args) conveniently
+erun() {
+    if [ $# -lt 2 ]; then
+        echo "Error: at least 2 args!"
+        return 1
+    fi
+    erl -s "$@" -s init stop -noshell
+}
+
+# Run erlang one-line script conveniently
+erline() {
+    if [ $# -ne 1 ]; then
+        echo "Error: Only need 1 arg!"
+        return 1
+    fi
+    erl -eval "$1" -s init stop -noshell
+}
+
 
 ###############################################################################
 # Lisp
@@ -262,193 +506,14 @@ alias bp='$HOME/ProgFiles/BProlog/bp'
 CLISP_DOC=/usr/local/Cellar/clisp/2.49/share/doc/clisp/doc
 
 alias schm='rlwrap -p 1\;32 -r -c -f $HOME/.scheme_completion.rlwrap scheme'
-alias scl='scala -Dscala.color -feature'
 
 ###############################################################################
-# Gradle
+# Prolog
 ###############################################################################
+alias sp='/Applications/SWI-Prolog.app/Contents/MacOS/swipl'
+alias gpl='gprolog'
+alias bp='$HOME/ProgFiles/BProlog/bp'
 
-#export GRADLE_OPTS="-Xmx1024m -Xms256m -XX:MaxPermSize=512m"
-# --daemon
-alias grd='gradle'
-function grw() {
-    local d="$PWD"
-    while true; do
-        [ "/" = "$d" ] && {
-            echo "fail to find gradlew" 2>&1
-            return 1
-        }
-        [ -f "$d/gradlew" ] && {
-            break
-        }
-        d=`dirname "$d"`
-    done
-
-    [ $d != $PWD ] && echo "use gradle wrapper: $(realpath "$d" --relative-to="$PWD")/gradlew"
-    "$d/gradlew" "$@"
-}
-alias grwf='grw --refresh-dependencies'
-alias grwb='grw build'
-alias grwfb='grw --refresh-dependencies build'
-alias grwc='grw clean'
-alias grwfc='grw --refresh-dependencies clean'
-alias grwcb='grw clean build'
-alias grwfcb='grw --refresh-dependencies clean build'
-
-alias grwt='grw test'
-alias grwd='grw -q dependencies'
-alias grwdc='grw -q dependencies --configuration compile'
-alias grwdr='grw -q dependencies --configuration runtime'
-alias grwdtc='grw -q dependencies --configuration testCompile'
-
-alias kgrdm="jps -l | awk '\$2==\"org.gradle.launcher.daemon.bootstrap.GradleDaemon\"{print \$1}' | xargs -r kill -9"
-alias sgrdm="jps -l | awk '\$2==\"org.gradle.launcher.daemon.bootstrap.GradleDaemon\"{print \$0}'"
-
-###############################################################################
-# Git
-###############################################################################
-compdef g=git
-
-alias gu='git up'
-gur() {
-    local d
-    local -a failedDirs=()
-    for d in `find -iname .git -type d`; do
-        d="$(readlink -f "$d/..")"
-        (
-            cd $d && {
-                echo
-                echo "================================================================================"
-                echo "$PWD"
-                echo "================================================================================"
-                git up
-            }
-        ) || failedDirs=( "$failedDirs[@]" "$d")
-    done
-    if [ "$#failedDirs[@]" -gt 0 ]; then
-        echo
-        echo
-        echo "================================================================================"
-        echo "Failed dirs:"
-        for d in "$failedDirs[@]"; do
-            echo "    $d"
-        done
-    fi
-}
-
-alias ga.='git add .'
-alias ga..='git add ..'
-alias gcoh='git checkout HEAD'
-
-# alias gd > /dev/null && unalias gd
-function gdc() {
-    git diff --cached --color "$@" | diff-so-fancy | less --tabs=1,5 -iRFX
-}
-function gds() {
-    if [ $# -eq 0 ]; then
-        from=@^
-        to=@
-    elif [ $# -eq 1 ]; then
-        from="$1^"
-        to="$1"
-    else
-        from="$1"
-        to="$2"
-    fi
-    git diff "$from" "$to"
-}
-alias gdh='git diff HEAD'
-alias gdorigin='git diff origin/$(git_current_branch)'
-
-alias gssi='git status -s --ignored'
-alias gsti='git status --ignored'
-alias gg='glog -15'
-alias sg='open -a /Applications/SmartGit.app'
-
-alias grb='git rebase'
-alias grs='git reset'
-alias grshd='git reset --hard'
-alias grsorigin='git reset --hard origin/$(git_current_branch)'
-
-alias gam='git commit --amend -v'
-alias gamno='git commit --amend --no-edit'
-
-alias ga.c='git add . && git commit -v'
-alias ga.m='git add . && git commit --amend -v'
-alias ga.mno='git add . && git commit --amend --no-edit'
-
-alias gpf='git push -f'
-alias ga.cp='git add . && git commit -v && git push'
-
-alias gampf='git commit --amend --no-edit && git push -f'
-alias ga.mpf='git add . && git commit --amend --no-edit && git push -f'
-
-alias gcn='git clone'
-alias gcnr='git clone --recurse-submodules'
-
-## Branch
-
-alias gbd='git branch -d'
-alias gbD='git branch -D'
-gbb() {
-    git branch -a "$@" | sed "/->/b; s#remotes/origin/#remotes/origin => #"
-}
-gbT() {
-    # http://stackoverflow.com/questions/5188320/how-can-i-get-a-list-of-git-branches-ordered-by-most-recent-commit
-    # --sort=-committerdate : sort branch by commit date in descending order
-    # --sort=committerdate : sort branch by commit date in ascending order
-    git branch -a --sort=committerdate "$@" | sed -r "/->/b; /\/tags\//d; /\/releases\//d; /\/backups?\//d; s#remotes/origin/#remotes/origin => #"
-}
-
-# http://stackoverflow.com/questions/1419623/how-to-list-branches-that-contain-a-given-commit
-alias gbc='git branch --contains'
-alias gbrc='git branch -r --contains'
-alias gbw='git browse'
-
-## URL
-
-# show swithed git repo addr(git/http)
-shg() {
-    local url="${1:-$(git remote get-url origin)}"
-    if [ -z "$url" ]; then
-        echo "No arguement and Not a git repository!"
-        return 1
-    fi
-
-    if [[ "$url" =~ '^http' ]]; then
-        echo "$url" | sed 's#^https?://#git@#; s#(\.com|\.org)/#\1:#; s#(\.git)?$#\.git#' -r | c
-    else
-        echo "$url" | sed 's#^git@#http://#; s#http://github.com#https://github.com#; s#(\.com|\.org):#\1/#; s#\.git$##' -r | c
-    fi
-}
-
-shgr() {
-    local d
-    for d in `find -iname .git -type d`; do
-        (
-            cd $d/..
-            echo "$PWD : $(git remote get-url origin)"
-        )
-    done
-}
-
-# change git repo addr http addr recursively
-chgr() {
-    local d
-    for d in `find -iname .git -type d`; do
-        (
-            cd $d/..
-            local url=$(git remote get-url origin)
-            [[ "$url" =~ '^http'  ]] && {
-                gitUrl=$(sgh)
-                echo "CHANGE $PWD : $url to $gitUrl"
-                grset origin $gitUrl
-            } || {
-                echo "Ignore $PWD : $url"
-            }
-        )
-    done
-}
 
 ###############################################################################
 # JetBrains
@@ -459,21 +524,19 @@ JB_TOOL_HOME="$HOME/Library/Application Support/JetBrains/Toolbox/apps"
 
 #alias idea='open -a /Applications/IntelliJ\ IDEA.app'
 alias idea='open -a "$JB_TOOL_HOME"/IDEA-U/*/*/IntelliJ*.app'
-#alias pych='open -a /Applications/PyCharm.app'
-alias pych='open -a "$JB_TOOL_HOME"/PyCharm-P/*/*/PyCharm*.app'
+
+alias wbs='open -a "$JB_TOOL_HOME"/WebStorm/*/*/WebStorm*.app'
+#alias pyc='open -a /Applications/PyCharm.app'
+alias pyc='open -a "$JB_TOOL_HOME"/PyCharm-P/*/*/PyCharm*.app'
+alias rbm='open -a "$JB_TOOL_HOME"/RubyMine/*/*/RubyMine*.app'
+
 #alias apcd='open -a /Applications/AppCode.app'
-alias apcd='open -a "$JB_TOOL_HOME"/AppCode/*/*/AppCode*.app'
-
-#alias cln='open -a /Applications/CLion.app'
-alias cln='open -a "$JB_TOOL_HOME"/CLion/*/*/CLion*.app'
-
-alias rdr='open -a "$JB_TOOL_HOME"/Rider/*/*/Rider*.app'
-alias mps='open -a "$JB_TOOL_HOME"/MPS/*/*/MPS*.app'
-
-#alias dtg='open -a /Applications/DataGrip.app'
-alias dtg='open -a "$JB_TOOL_HOME"/datagrip/*/*/DataGrip*.app'
-
-alias wbs='open -a /Applications/WebStorm.app'
-alias phs='open -a /Applications/PhpStorm.app'
-
+alias apc='open -a "$JB_TOOL_HOME"/AppCode/*/*/AppCode*.app'
 alias ads='open -a /Applications/Android\ Studio.app'
+
+alias cln='open -a "$JB_TOOL_HOME"/CLion/*/*/CLion*.app'
+alias rdr='open -a "$JB_TOOL_HOME"/Rider/*/*/Rider*.app'
+
+alias dtg='open -a "$JB_TOOL_HOME"/datagrip/*/*/DataGrip*.app'
+#alias phs='open -a /Applications/PhpStorm.app'
+alias mps='open -a "$JB_TOOL_HOME"/MPS/*/*/MPS*.app'
