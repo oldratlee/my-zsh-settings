da8c28a68063ad2f2a49bd47757652689be789f8
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-20 19:52:35 +0800

    Delete components directory

--- components/14-ios.zsh
+++
@@ -1,8 +0,0 @@
-# iOS开发环境准备
-# https://yuque.antfin-inc.com/taobaoenv/taobaoenv/install
-
-#export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
-command -v rbenv &> /dev/null &&
-eval "$(rbenv init -)"
-
-[ -f ~/.tbenv/bundler-exec.sh ] && source ~/.tbenv/bundler-exec.sh
